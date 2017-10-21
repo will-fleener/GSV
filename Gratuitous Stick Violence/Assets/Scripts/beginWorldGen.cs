6539edcbe561ef4f2c6d98a8f028b1a10f66b2b5
Unknown <hharrisonw@gmail.com>
2017-10-20 22:17:15 -0500

    Auto stash before rebase of "origin/master"
    Made the menu screen, got the buttons working

---
+++ Gratuitous Stick Violence/Assets/Scripts/beginWorldGen.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class beginWorldGen : MonoBehaviour {
+
+    private Button btn;
+
+    // Use this for initialization
+    void Start()
+    {
+        btn = this.GetComponent<Button>();
+        btn.onClick.AddListener(commenceWorldGen);
+
+    }
+
+    void commenceWorldGen()
+    {
+        //Replace this with the async version and a nice loading screen
+        //I'm talking to you
+        //Do it
+        //Do it now
+        SceneManager.LoadScene("Game", LoadSceneMode.Single);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
