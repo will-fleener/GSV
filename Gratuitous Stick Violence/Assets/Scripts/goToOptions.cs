a5fdfb0a282a7223bbd7ca073a30e0ea1586b707
Unknown <hharrisonw@gmail.com>
2017-10-20 22:17:15 -0500

    Made the menu screen, got the buttons working
    I still don't know what a rebase is
    send help

---
+++ Gratuitous Stick Violence/Assets/Scripts/goToOptions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class goToOptions : MonoBehaviour {
+
+    private Button btn;
+
+	// Use this for initialization
+	void Start () {
+        btn = this.GetComponent<Button>();
+        btn.onClick.AddListener(showOptionsMenu);
+        		
+	}
+	
+    void showOptionsMenu ()
+    {
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
