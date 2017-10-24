42b64cae09d242083fa7b7a8cc73a40f98d7dc7f
Will <avatarwill5@gmail.com>
2017-10-24 18:10:03 -0500

    Added extremely basic player character that continiously moves to the right and can "jump" with up arrow

---
+++ Gratuitous Stick Violence/Assets/PlayerCharacterControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacterControl : MonoBehaviour {
+    public float moveForce = 300f;
+    public float maxSpeed = 1f;
+    public float jumpForce = 100f;
+
+    private Rigidbody2D rb;
+
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        rb.velocity = Vector3.right * maxSpeed;
+
+        float v = Input.GetAxis("Vertical");
+        if (v * rb.velocity.y < maxSpeed)
+        {
+            rb.AddForce(Vector2.up *v * jumpForce);
+        }
+        
+	}
+}
