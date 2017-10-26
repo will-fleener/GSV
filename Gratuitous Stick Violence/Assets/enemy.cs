ad9281d7e29812b28a5da741ad7388c3ff571248
Annie Hua <anniehua96@gmail.com>
2017-10-26 18:10:00 -0500

    Added enemy

---
+++ Gratuitous Stick Violence/Assets/enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour {
+
+	public float movespeed;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (-Vector2.right * movespeed * Time.deltaTime);
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+
+		if (col.gameObject.tag == "Player") {
+			Destroy (col.gameObject);
+		}
+	}
+}
