9aa3e2adcba618c97ec3e10269ffcfc52bf7cba2
Unknown <hharrisonw@gmail.com>
2017-10-27 23:15:52 -0500

    Added attack animation and fixed attacks

--- Gratuitous Stick Violence/Assets/enemy.cs
+++ Gratuitous Stick Violence/Assets/enemy.cs
@@ -35,15 +35,17 @@ public class enemy : MonoBehaviour
 
     }
 
-    void Die()
+    public void Die()
     {
         //death animation
         print("enemy die animation");
         //Physics2D.IgnoreCollision(_archibaldCol, _player.GetComponent<Collider2D>());
         _archibaldCol.enabled = false;
         _archibaldRB.isKinematic = true; ;
-        
-        _archibaldAnim.SetTrigger("Get Shanked");
+        _archibaldRB.velocity = Vector2.zero;
+
+        //_archibaldAnim.SetTrigger("Get Shanked");
+        _archibaldAnim.Play("archibald_ded");
         StartCoroutine(DoubleDie());
     }
 
