646a7f649ba4b1bf743dce6af718fecc01ae6a7b
Will <avatarwill5@gmail.com>
2017-10-27 23:05:07 -0500

    Implemented high score saving and scoreboard

---
+++ Gratuitous Stick Violence/Assets/toMainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class toMainMenu : MonoBehaviour {
+   private Button _btn;
+    // Use this for initialization
+    void Start () {
+        _btn = this.GetComponent<Button>();
+        _btn.onClick.AddListener(goToMainMenu);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+     void goToMainMenu()
+    {
+        SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+    }
+     void printStuff()
+    {
+        print("stuff");
+    }
+}
