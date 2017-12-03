d5e68a631e4abac742cfd6ff76a2500ae8f3901c
Unknown <rand-y@outlook.com>
2017-12-02 21:33:49 -0600

    Added better platform generation

--- Gratuitous Stick Violence/Assets/Scripts/platformGenerator.cs
+++ Gratuitous Stick Violence/Assets/Scripts/platformGenerator.cs
@@ -12,19 +12,34 @@ public class platformGenerator : MonoBehaviour {
 
     private float platformWidth;
 
+	private float minHeight;
+	public Transform maxHeightPoint;
+	private float maxHeight;
+	public float maxHeightChange;
+	private float heightChange;
+	
  
 
 	// Use this for initialization
 	void Start () {
         platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;
 		
+		minHeight = transform.position.y;
+		maxHeight = maxHeightPoint.position.y;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(transform.position.x < generationPoint.position.x){
             distanceBetween = Random.Range(distanceBetweenMin, distanceBetweenMax);
-            transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
+			heightChange = transform.position.y + Random.Range(maxHeightChange, -maxHeightChange);
+            
+			if(heightChange > maxHeight){
+				heightChange = maxHeight;
+			}else if(heightChange < minHeight){
+				heightChange = minHeight;
+			}
+			transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, heightChange, transform.position.z);
             Instantiate(thePlatform, transform.position, transform.rotation);
         }
 	}
